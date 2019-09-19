86c207716ed8ec22997217416178e613643e090d
Tish Chungoora <tishal82@gmail.com>
2019-09-19 10:43:52 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+=begin
+# Using .each method and results are as expected in IRB
+
+def reverse_each_word(string)
+  arr = string.split
+  arr.each { |n| n.reverse!}.join(" ")
+end
+=end
+
+# Using .collect method and results
+
+def reverse_each_word(string)
+  arr = string.split
+  arr.collect { |n| n.reverse!}.join(" ")
+end
